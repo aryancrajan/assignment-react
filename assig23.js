fdcb696932b1743fe41f8cd2cc79e15aad4bd536
ARYAN C R <57360045+aryancrajan@users.noreply.github.com>
2023-07-12 16:59:08 +0530

    assignment23

--- assig23.js
+++ assig23.js
@@ -2,14 +2,11 @@ const a=[[1,2,3],[4,5,6],[7,8,9]];
 var s1=0;
 var s2=0;
 var j
-for(let i=0;i<a.length;i++)
+for(let i=0;i<a.length;i++) 
     {
-        s1=s1+a[i][i];   
-    }
-for(let k=0;k<a.length;k++) 
-    {
-        j=(a[k].length-k-1);
-        s2=s2+a[k][j];
+        s1=s1+a[i][i];
+        j=(a[i].length-i-1);
+        s2=s2+a[i][j];
     }
 console.log('First diagonal sum:', s1);
 console.log('Second diagonal sum:',s2);
