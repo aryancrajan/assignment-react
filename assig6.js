02d7e04fc364b0dfa2f59beb288392731a19955c
ARYAN C R <57360045+aryancrajan@users.noreply.github.com>
2023-07-12 12:20:33 +0530

    assignment6

--- assig6.js
+++ assig6.js
@@ -1,4 +1,4 @@
-console.log('You bought 200g of p1 rated 40rs/MediaKeyMessageEvent,650g of p2 rated 25rs/kg and 2kg of p3 rated 35rs/kg.');
+console.log('You bought 200g of p1 rated 40rs/kg,650g of p2 rated 25rs/kg and 2kg of p3 rated 35rs/kg.');
 
 let p1=40,p2=25,p3=35;
 let q1=.200,q2=.650,q3=2;
